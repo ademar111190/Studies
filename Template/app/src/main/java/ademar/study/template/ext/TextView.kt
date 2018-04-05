ba65fba2e73d9e3a7998fd0a2ef2a4202b1db65a
Ademar Alves de Oliveira <ademar.oliveira@wizeline.com>
2018-04-05 18:25:32 -0500

    Delete unused extensions

--- Template/app/src/main/java/ademar/study/template/ext/TextView.kt
+++
@@ -1,8 +0,0 @@
-package ademar.study.template.ext
-
-import android.graphics.Paint
-import android.widget.TextView
-
-fun TextView.strike() {
-    paintFlags = paintFlags or Paint.STRIKE_THRU_TEXT_FLAG
-}
--- Template/core/src/main/java/ademar/study/template/core/ext/CharSequence.kt
+++ Template/core/src/main/java/ademar/study/template/core/ext/CharSequence.kt
@@ -1,15 +1,3 @@
 package ademar.study.template.core.ext
 
-import android.os.Build
-import android.text.Html
-
-fun CharSequence.extractNumbers() = this.toString().replace("[^0-9]".toRegex(), "")
-
-fun CharSequence.toHtml() = if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.N) {
-    Html.fromHtml(toString(), android.text.Html.FROM_HTML_MODE_LEGACY)
-} else {
-    @Suppress("DEPRECATION")
-    Html.fromHtml(toString())
-}
-
-fun CharSequence.extractHtmlContent() = toHtml().toString()
+fun CharSequence.extractNumbers() = toString().replace("[^0-9]".toRegex(), "")
