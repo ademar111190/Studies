0755558ba81ee6cb0d73d5a1140a12bcad692f3e
Ademar Oliveira <ademar@porquenao.mobi>
2018-09-27 10:40:09 -0300

    Initial fixes

--- Template/app/src/androidTest/java/ademar/study/template/ExampleInstrumentedTest.kt
+++ Template/app/src/androidTest/java/ademar/study/template/ExampleInstrumentedTest.kt
@@ -2,23 +2,17 @@ package ademar.study.template
 
 import androidx.test.InstrumentationRegistry
 import androidx.test.runner.AndroidJUnit4
-
+import org.junit.Assert.assertEquals
 import org.junit.Test
 import org.junit.runner.RunWith
 
-import org.junit.Assert.*
-
-/**
- * Instrumented test, which will execute on an Android device.
- *
- * See [testing documentation](http://d.android.com/tools/testing).
- */
 @RunWith(AndroidJUnit4::class)
 class ExampleInstrumentedTest {
+
     @Test
     fun useAppContext() {
-        // Context of the app under test.
         val appContext = InstrumentationRegistry.getTargetContext()
         assertEquals("ademar.study.template", appContext.packageName)
     }
+
 }
