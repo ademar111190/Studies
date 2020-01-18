aa6ac441bbe8e19d1418ea74e54901af508fcc88
Ademar Oliveira <ademar111190@gmail.com>
2020-01-18 12:40:09 -0300

    Init android project

---
+++ Template/app/src/test/java/study/template/ExampleUnitTest.kt
@@ -0,0 +1,13 @@
+package study.template
+
+import org.junit.Assert.assertEquals
+import org.junit.Test
+
+class ExampleUnitTest {
+
+    @Test
+    fun addition_isCorrect() {
+        assertEquals(4, 2 + 2)
+    }
+
+}
