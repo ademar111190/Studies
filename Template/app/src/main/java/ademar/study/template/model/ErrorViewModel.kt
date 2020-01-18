03e476dd9798883de80fc7a61cc04d372a3376ae
Ademar Oliveira <ademar111190@gmail.com>
2020-01-18 11:49:29 -0300

    Remove old stuff

--- Template/app/src/main/java/ademar/study/template/model/ErrorViewModel.kt
+++
@@ -1,7 +0,0 @@
-package ademar.study.template.model
-
-data class ErrorViewModel(
-
-        val message: String
-
-)
--- Template/core/src/test/java/ademar/study/template/core/test/AbstractCharSequenceAssert.kt
+++
@@ -1,6 +0,0 @@
-package ademar.study.template.core.test
-
-import org.assertj.core.api.AbstractCharSequenceAssert
-
-fun AbstractCharSequenceAssert<*, String>.containsJson(key: String, value: String)
-        : AbstractCharSequenceAssert<*, *> = contains("\"$key\":\"$value\"")
