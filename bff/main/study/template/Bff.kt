3c078ecd81a38dec6c6b76aa08b0c801062fe30c
Ademar Oliveira <ademar111190@gmail.com>
2020-02-25 00:25:09 -0300

    Bff hello world

--- bff/main/study/template/Bff.kt
+++ bff/main/study/template/Bff.kt
@@ -1,7 +1,23 @@
 package study.template
 
+import io.ktor.application.call
+import io.ktor.http.ContentType
+import io.ktor.response.respondText
+import io.ktor.routing.get
+import io.ktor.routing.routing
 import io.ktor.server.engine.embeddedServer
+import io.ktor.server.netty.Netty
 
 fun main(args: Array<String>) {
-    println("Hello World")
+    val server = embeddedServer(Netty, port = 8080) {
+        routing {
+            get("/") {
+                call.respondText("Hello World!", ContentType.Text.Plain)
+            }
+            get("/demo") {
+                call.respondText("HELLO WORLD!")
+            }
+        }
+    }
+    server.start(wait = true)
 }
